ea4eb6c6c3f2350754c02ffe1c6c3fe82b712aa3
Brij Vachhani <brijvachhani2311@gmail.com>
2022-12-03 12:57:19 +0100

    Updated setup.py file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name="foo",
+    name="snowflake",
     version="1.0",
     packages=find_packages(),
     install_requires = required
